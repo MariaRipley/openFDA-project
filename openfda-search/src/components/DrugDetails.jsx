487d980982a7556bb8cfe8fa669424236e99b4a7
AstracodeLAB <astracodelab@gmail.com>
2024-06-17 17:27:39 +0200

    Add more fields to Drug Details

--- openfda-search/src/components/DrugDetails.jsx
+++ openfda-search/src/components/DrugDetails.jsx
@@ -1,4 +1,4 @@
-import { Box, Typography, Button } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchDetail } from '../api/openFDA';
@@ -20,7 +20,30 @@ function DrugDetails() {
 			<Box>
 				{drug ? (
 					<>
-						<Typography>{drug.openfda.brand_name ? drug.openfda.brand_name[0] : 'Unknown'}</Typography>
+						<Typography>Brand name: {drug.openfda.brand_name ? drug.openfda.brand_name[0] : 'Unknown'}</Typography>
+						<Typography>
+							Generic name: {drug.openfda.generic_name ? drug.openfda.generic_name[0] : 'Unknown'}
+						</Typography>
+						<Typography>
+							Manufacturer name: {drug.openfda.manufacturer_name ? drug.openfda.manufacturer_name[0] : 'Unknown'}
+						</Typography>
+						<Typography>Description: {drug.description || 'No description found'}</Typography>
+						<Typography>Purpose: {drug.purpose || 'No purpose found'}</Typography>
+						<Typography>
+							Active ingredients: {drug.products?.[0]?.active_ingredients?.[0]?.name || 'No active ingredients found'}
+						</Typography>
+						<Typography>Dosage form: {drug.openfda?.dosage_form || 'No dosage form found'}</Typography>
+						<Typography>Warnings: {drug.warnings || 'No warnings found'}</Typography>
+						<Typography>
+							Applications form URL:
+							{drug.openfda?.applications_form_url ? (
+								<a href={drug.openfda.applications_form_url} target='_blank' rel='noopener noreferrer'>
+									{drug.openfda.applications_form_url}
+								</a>
+							) : (
+								'No applications form URL found'
+							)}
+						</Typography>
 					</>
 				) : (
 					<Typography>Loading...</Typography>
