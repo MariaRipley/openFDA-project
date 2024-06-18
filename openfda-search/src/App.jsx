1081cc420be80ea8bd8d14c61bd0d57b948c30ce
AstracodeLAB <astracodelab@gmail.com>
2024-06-18 11:46:08 +0200

    Add SearchProvider to App.jsx

--- openfda-search/src/App.jsx
+++ openfda-search/src/App.jsx
@@ -1,16 +1,18 @@
-// App.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import DrugDetails from './components/DrugDetails';
+import { SearchProvider } from './context/SearchContext';
 
 function App() {
 	return (
-		<Router>
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/drug/:id' element={<DrugDetails />} />
-			</Routes>
-		</Router>
+		<SearchProvider>
+			<Router>
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/drug/:id' element={<DrugDetails />} />
+				</Routes>
+			</Router>
+		</SearchProvider>
 	);
 }
 
