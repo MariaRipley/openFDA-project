47f1ae229b71664bb4fd9fe8237f3a7c0a3bbe79
AstracodeLAB <astracodelab@gmail.com>
2024-06-18 11:44:51 +0200

    Add SearchProvider function

--- openfda-search/src/context/SearchContext.jsx
+++ openfda-search/src/context/SearchContext.jsx
@@ -1,3 +1,14 @@
 import { createContext, useState } from 'react';
 
 export const SearchContext = createContext();
+
+export const SearchProvider = ({ children }) => {
+	const [searchResults, setSearchResults] = useState([]);
+	const [query, setQuery] = useState('');
+
+	return (
+		<SearchContext.Provider value={{ searchResults, setSearchResults, query, setQuery }}>
+			{children}
+		</SearchContext.Provider>
+	);
+};
